24db09792401368bd64e30f22601899101c296e5
Anton Tananaev <anton.tananaev@gmail.com>
2016-01-13 22:25:51 +1300

    Make login activity the launcher activity

--- app/src/main/java/org/traccar/manager/LoginActivity.java
+++ app/src/main/java/org/traccar/manager/LoginActivity.java
@@ -15,8 +15,22 @@
  */
 package org.traccar.manager;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.content_layout, new LoginFragment())
+                    .commit();
+        }
+    }
 
 }
--- app/src/main/java/org/traccar/manager/LoginFragment.java
+++ app/src/main/java/org/traccar/manager/LoginFragment.java
@@ -15,8 +15,19 @@
  */
 package org.traccar.manager;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
 
 }
