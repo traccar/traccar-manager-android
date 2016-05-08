44685b4398ed98af3cc29b5b1479fbd7d44ae745
Gábor Somogyi <gabor.g.somogyi@gmail.com>
2016-05-08 22:32:43 +0200

    Remember me function

--- app/src/main/java/org/traccar/manager/LoginFragment.java
+++ app/src/main/java/org/traccar/manager/LoginFragment.java
@@ -73,6 +73,9 @@ public class LoginFragment extends Fragment {
 
         final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
 
+        emailInput.setText(preferences.getString(MainApplication.PREFERENCE_EMAIL, null));
+        passwordInput.setText(preferences.getString(MainApplication.PREFERENCE_PASSWORD, null));
+
         if (preferences.getBoolean(MainApplication.PREFERENCE_AUTHENTICATED, false)) {
             startMainActivity();
         }
