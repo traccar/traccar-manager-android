dfbb36968c4186b8e0c70c47486ef85b14e8c8ba
Gábor Somogyi <gabor.g.somogyi@gmail.com>
2016-06-03 20:00:13 +0200

    Add new device possibility

--- app/src/main/java/org/traccar/manager/DevicesFragment.java
+++ app/src/main/java/org/traccar/manager/DevicesFragment.java
@@ -19,15 +19,18 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.PopupMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import org.traccar.manager.model.Device;
 
 import java.util.List;
@@ -54,6 +57,24 @@ public class DevicesFragment extends ListFragment implements View.OnClickListene
             popupText.setOnClickListener(DevicesFragment.this);
             return view;
         }
+    }
+
+    private FloatingActionButton addButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        addButton = (FloatingActionButton) view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startEditDeviceActivity(0);
+            }
+        });
+
+        return view;
     }
 
     @Override
